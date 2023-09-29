8267988b119f5420327a648ac29acdb59d3a3e7b
Devpuns <punsisi96munasinha@gmail.com>
2023-09-29 22:30:09 +0530

    Next Update 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,23 @@
+// function goBtnOnClicked() {
+//     const searchInput = document.querySelector('.form-control'); // Assuming this is your input field
+//     const cityName = document.querySelector('#cityName');
+//     const city = searchInput.value;
+//     cityName.innerText = city;
+//     searchInput.value = "";
+//     console.log(cityName.innerText);
+// }
 function goBtnOnClicked() {
-    const searchInput = document.querySelector('.form-control'); // Assuming this is your input field
-    const cityName = document.querySelector('#cityName');
-    const city = searchInput.value;
-    cityName.innerText = city;
-    searchInput.value = "";
-    console.log(cityName.innerText);
+    const httpReq = new XMLHttpRequest();
+    httpReq.open("GET","db/db.txt");
+    // console.log("Request Created : "+httpReq.readyState);
+    httpReq.send();
+    // console.log("request sent : "+httpReq.readyState);
+    httpReq.onreadystatechange = () => {
+        if(httpReq.readyState == 4){
+            console.log(httpReq.responseText);
+        }
+        //  console.log('Ready State value :'+httpReq.readyState);
+    }
+    console.log(httpReq.responseText);
+    // console.log("response recieved : "+httpReq.readyState);
 }
